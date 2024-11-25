4c8ff78d0dac724af7a03528d765bfcb7da35766
Samiul Islam <samiulislamsharan@gmail.com>
2024-11-26 00:12:23 +0600

    feat(routes): :sparkles: add authentication routes for user sign-in and logout

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Auth\AuthController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::controller(AuthController::class)->group(function () {
+    Route::get('/sign-in', 'index')->name('login.index');
+    Route::post('/login', 'loginUser')->name('loginPost');
+    Route::get('/logout', 'logout')->name('logout');
 });
