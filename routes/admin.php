aa8803bc229a81a78b66a3a190da36b4ea053f7e
Samiul Islam <samiulislamsharan@gmail.com>
2024-11-26 22:57:59 +0600

    feat(routes): :sparkles: add profile routes for viewing and saving admin profile

--- routes/admin.php
+++ routes/admin.php
@@ -1,6 +1,10 @@
 <?php
 
 use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Admin\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/dashboard', [DashboardController::class, 'index'])->name('admin.dashboard.index');
+
+Route::get('/profile', [ProfileController::class, 'index'])->name('admin.profile.index');
+Route::post('/save-profile', [ProfileController::class, 'storeUser'])->name('admin.profile.store');
