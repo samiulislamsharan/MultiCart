e275b7ec3581739d82ad0d96582423630572cb3e
Samiul Islam <samiulislamsharan@gmail.com>
2024-11-26 22:50:05 +0600

    feat(routes): :sparkles: update dashboard route to use DashboardController for improved structure

--- routes/admin.php
+++ routes/admin.php
@@ -1,7 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\DashboardController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/dashboard', function () {
-    return view('admin/index');
-});
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('admin.dashboard.index');
