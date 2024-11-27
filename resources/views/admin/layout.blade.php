f6c323553f0ef8919f61f9be2c652c69bd451de0
Samiul Islam <samiulislamsharan@gmail.com>
2024-11-27 18:49:51 +0600

    fix(views): :bug: update user avatar image source to dynamically use authenticated user's image

--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -389,7 +389,7 @@
                     <div class="user-box dropdown">
                         <a class="d-flex align-items-center nav-link dropdown-toggle dropdown-toggle-nocaret"
                             href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            <img src="{{ asset('assets/images/avatars/avatar-2.png') }}" class="user-img"
+                            <img src="{{ asset(auth()->user()->image) }}" class="user-img"
                                 alt="user avatar">
                             <div class="user-info ps-3">
                                 <p class="user-name mb-0">{{ auth()->user()->name }}</p>
