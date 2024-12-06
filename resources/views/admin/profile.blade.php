26bdd9ccf6759ee340a24c37df78bef083bdbe5a
Samiul Islam <samiulislamsharan@gmail.com>
2024-12-07 00:05:47 +0600

    fix(views): :bug: adjust spacing of horizontal rule in admin profile view

--- resources/views/admin/profile.blade.php
+++ resources/views/admin/profile.blade.php
@@ -56,7 +56,7 @@
                                             <button class="btn btn-outline-primary">Message</button>
                                         </div>
                                     </div>
-                                    <hr class="my-4" />
+                                    <hr class="my-2" />
                                     <ul class="list-group list-group-flush">
                                         <li
                                             class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
