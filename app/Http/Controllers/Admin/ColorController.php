b66461c33ae66f9893a06719ce311d2e14ff85ca
Samiul Islam <samiulislamsharan@gmail.com>
2024-12-09 13:15:17 +0600

    feat(controller): :sparkles: add validation and error handling in store method for color creation and updates

--- app/Http/Controllers/Admin/ColorController.php
+++ app/Http/Controllers/Admin/ColorController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\Color;
 use App\Traits\ApiResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class ColorController extends Controller
 {
@@ -34,7 +35,29 @@ class ColorController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        try {
+            $validation = Validator::make($request->all(), [
+                'id' => 'required',
+                'text' => 'required|string|max:255',
+                'value' => 'required|string|max:255',
+            ]);
+
+            if ($validation->fails()) {
+                return $this->error($validation->errors(), 422, []);
+            } else {
+                Color::updateOrCreate(
+                    ['id' => $request->id],
+                    [
+                        'text' => $request->text,
+                        'value' => $request->value,
+                    ]
+                );
+
+                return $this->success(['reload' => true], 'Color updated successfully.');
+            }
+        } catch (\Exception $e) {
+            return $this->error($e->getMessage(), 500, []);
+        }
     }
 
     /**
