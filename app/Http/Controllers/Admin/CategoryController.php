03cc5f6ccb9f18f28d16b099e4953d5f94ac3d2e
Samiul Islam <samiulislamsharan@gmail.com>
2024-12-15 19:20:37 +0600

    feat(controllers): :sparkles: implement store method with image upload and validation for categories

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -5,11 +5,13 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Models\Category;
 use App\Traits\ApiResponse;
+use App\Traits\SaveFile;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class CategoryController extends Controller
 {
-    use ApiResponse;
+    use ApiResponse, SaveFile;
 
     /**
      * Display a listing of the resource.
@@ -34,7 +36,44 @@ class CategoryController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        try {
+            $validation = Validator::make($request->all(), [
+                'id' => 'required',
+                'name' => 'required|string|max:255',
+                'slug' => 'required|string|max:255',
+                'image' => 'image|mimes:jpeg,png,jpg,gif,svg|max:5120',
+                'parent_category_id' => 'nullable|integer',
+            ]);
+
+            if ($validation->fails()) {
+                return $this->error($validation->errors(), 422, []);
+            } else {
+                if ($request->hasFile('image')) {
+                    if ($request->id > 0) {
+                        $image = Category::find($request->id)->image;
+                        $image = $this->saveImage($request->image, $image, 'images/categories');
+                    } else {
+                        $image = $this->saveImage($request->image, null, 'images/categories');
+                    }
+                } else {
+                    $image = Category::where('id', $request->id)->pluck('image')->first();
+                }
+
+                Category::updateOrCreate(
+                    ['id' => $request->id],
+                    [
+                        'name' => $request->name,
+                        'slug' => $request->slug,
+                        'image' => $image,
+                        'parent_category_id' => $request->parent_category_id,
+                    ]
+                );
+
+                return $this->success(['reload' => true], 'Attribute name updated successfully.');
+            }
+        } catch (\Exception $e) {
+            return $this->error($e->getMessage(), 500, []);
+        }
     }
 
     /**
