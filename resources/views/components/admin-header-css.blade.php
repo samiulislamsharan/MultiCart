e8a0c48f343f372fd25c2ba594beb5b3596a333a
Samiul Islam <samiulislamsharan@gmail.com>
2024-12-20 16:56:48 +0600

    feat(views): :sparkles: add Trumbowyg CSS to admin header for rich text editing

--- resources/views/components/admin-header-css.blade.php
+++ resources/views/components/admin-header-css.blade.php
@@ -12,6 +12,7 @@
     <link href="{{ asset('assets/plugins/notifications/css/lobibox.css') }}" rel="stylesheet" />
     <link href="{{ asset('assets/plugins/datatable/css/dataTables.bootstrap5.min.css') }}" rel="stylesheet" />
     <link href="{{ asset('assets/plugins/Drag-And-Drop/dist/imageuploadify.min.css') }}" rel="stylesheet" />
+    <link href="{{ asset('assets/plugins/trumbowyg/ui/trumbowyg.min.css') }}" rel="stylesheet" />
     <!-- loader-->
     <link href="{{ asset('assets/css/pace.min.css') }}" rel="stylesheet" />
     <script src="{{ asset('assets/js/pace.min.js') }}"></script>
