ca87c26139b6890804602d8492049e3098c90af9
Samiul Islam <samiulislamsharan@gmail.com>
2025-01-28 02:49:24 +0600

    feat(models): :sparkles: add accessor for image attribute to return full URL in Category model

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Support\Facades\URL;
 
 class Category extends Model
 {
@@ -15,4 +17,11 @@ class Category extends Model
         'image',
         'parent_category_id'
     ];
+
+    protected function Image(): Attribute
+    {
+        return Attribute::make(function ($value) {
+            return URL::to($value);
+        });
+    }
 }
