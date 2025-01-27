2beae14bb7fc09e357661fe99e902183d573ddec
Samiul Islam <samiulislamsharan@gmail.com>
2025-01-28 02:43:02 +0600

    feat(models): :sparkles: add accessor for image attribute to return full URL

--- app/Models/Brand.php
+++ app/Models/Brand.php
@@ -4,10 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Support\Facades\URL;
 
 class Brand extends Model
 {
     use HasFactory;
 
     protected $fillable = ['text', 'image'];
+
+    protected function Image(): Attribute
+    {
+        return Attribute::make(function ($value) {
+            return URL::to($value);
+        });
+    }
 }
