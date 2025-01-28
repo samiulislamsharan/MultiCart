e3ce54352291d52e64b734b7190e1c1dd38f5b50
Samiul Islam <samiulislamsharan@gmail.com>
2025-01-28 23:49:32 +0600

    feat(controllers): :sparkles: create HomePageController to fetch home data including banners, categories, and brands

---
+++ app/Http/Controllers/Front/HomePageController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers\Front;
+
+use App\Http\Controllers\Controller;
+use App\Models\Brand;
+use App\Models\Category;
+use App\Models\HomeBanner;
+use App\Traits\ApiResponse;
+use Illuminate\Http\Request;
+
+class HomePageController extends Controller
+{
+    use ApiResponse;
+
+    public function index()
+    {
+        $data = [];
+        $data['banner'] = HomeBanner::get();
+        $data['categories'] = Category::with('products')->get();
+        $data['brands'] = Brand::get();
+
+        return $this->success(['data' => $data], 'Home Data fetched successfully');
+    }
+}
