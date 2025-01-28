490c1ca41ae8e217d48832c72a179dbec82c52ad
Samiul Islam <samiulislamsharan@gmail.com>
2025-01-28 16:49:08 +0600

    feat(models): :sparkles: add accessor to return full URL for image attribute in ProductAttrImages model

--- app/Models/ProductAttrImages.php
+++ app/Models/ProductAttrImages.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Support\Facades\URL;
 
 class ProductAttrImages extends Model
 {
@@ -14,4 +16,11 @@ class ProductAttrImages extends Model
         'product_attr_id',
         'image',
     ];
+
+    protected function Image(): Attribute
+    {
+        return Attribute::make(function ($value) {
+            return URL::to($value);
+        });
+    }
 }
