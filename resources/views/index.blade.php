9cc632f55778749333ac913e46215d85eb55c1f3
Samiul Islam <samiulislamsharan@gmail.com>
2025-01-29 16:09:27 +0600

    feat(views): :sparkles: add index.blade.php for the main landing page layout

---
+++ resources/views/index.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>MultiCart</title>
+
+    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('front_assets/img/favicon.png') }}">
+
+    <!-- CSS here -->
+    <link rel="stylesheet" href="{{ asset('front_assets/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('front_assets/css/animate.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('front_assets/css/magnific-popup.css') }}">
+    <link rel="stylesheet" href="{{ asset('front_assets/css/fontawesome-all.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('front_assets/css/jquery.mCustomScrollbar.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('front_assets/css/bootstrap-datepicker.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('front_assets/css/swiper-bundle.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('front_assets/css/jquery-ui.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('front_assets/css/nice-select.css') }}">
+    <link rel="stylesheet" href="{{ asset('front_assets/css/jarallax.css') }}">
+    <link rel="stylesheet" href="{{ asset('front_assets/css/flaticon.css') }}">
+    <link rel="stylesheet" href="{{ asset('front_assets/css/slick.css') }}">
+    <link rel="stylesheet" href="{{ asset('front_assets/css/default.css') }}">
+    <link rel="stylesheet" href="{{ asset('front_assets/css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('front_assets/css/responsive.css') }}">
+    <script src="{{ asset('front_assets/js/vendor/jquery-3.5.0.min.js') }}"></script>
+</head>
+
+<body>
+    @vite('resources/css/app.css')
+    <div id="app"></div>
+    @vite('resources/js/app.js')
+</body>
+
+</html>
