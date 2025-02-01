08504cec6b402f97176268d860d44ae06485eea7
Samiul Islam <samiulislamsharan@gmail.com>
2025-02-01 23:06:04 +0600

    feat(views): :sparkles: update slider component to dynamically render items from homeBanner in Index.vue

--- resources/js/frontend/Index.vue
+++ resources/js/frontend/Index.vue
@@ -4,7 +4,7 @@
             <!-- slider-area -->
             <section class="slider-area position-relative">
                 <div class="third-slider-active">
-                    <div class="third-slider-item third-slider-bg"
+                    <div v-for="item in homeBanner" :key="item.id" class="third-slider-item third-slider-bg"
                         data-background="front_assets/img/slider/third_slider_bg.jpg">
                         <div class="container custom-container-two">
                             <div class="third-slider-wrap">
@@ -14,11 +14,8 @@
                                             <h3 class="sub-title" data-animation-in="fadeInUp" data-delay-in=".2"
                                                 data-duration-in="1.5">best offer !</h3>
                                             <h2 class="title" data-animation-in="fadeInUp" data-delay-in=".4"
-                                                data-duration-in="1.5">Gym sports shoes</h2>
-                                            <p data-animation-in="fadeInUp" data-delay-in=".6" data-duration-in="1.5">
-                                                Get up
-                                                to 50% off Today Only</p>
-                                            <a href="shop-sidebar.html" class="btn" data-animation-in="fadeInUp"
+                                                data-duration-in="1.5">{{ item.text }}</h2>
+                                            <a :href="item.link" class="btn" data-animation-in="fadeInUp"
                                                 data-delay-in=".8" data-duration-in="1.5">Shop now</a>
                                         </div>
                                     </div>
@@ -28,107 +25,8 @@
                                                 data-background="front_assets/img/slider/third_slide_shape.png"
                                                 data-animation-in="zoomIn" data-delay-in="1" data-duration-in="1.5">
                                             </div>
-                                            <img src="front_assets/img/slider/third_slider_img01.png" alt=""
-                                                class="main-img" data-animation-in="slideInRight2" data-delay-in="1"
-                                                data-duration-in="1.5">
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="third-slider-item third-slider-bg"
-                        data-background="front_assets/img/slider/third_slider_bg.jpg">
-                        <div class="container custom-container-two">
-                            <div class="third-slider-wrap">
-                                <div class="row align-items-center">
-                                    <div class="col-lg-6">
-                                        <div class="slider-content">
-                                            <h3 class="sub-title" data-animation-in="fadeInUp" data-delay-in=".2"
-                                                data-duration-in="1.5">top deal !</h3>
-                                            <h2 class="title" data-animation-in="fadeInUp" data-delay-in=".4"
-                                                data-duration-in="1.5">athletes shoes</h2>
-                                            <p data-animation-in="fadeInUp" data-delay-in=".6" data-duration-in="1.5">
-                                                Get up
-                                                to 50% off Today Only</p>
-                                            <a href="shop-sidebar.html" class="btn" data-animation-in="fadeInUp"
-                                                data-delay-in=".8" data-duration-in="1.5">Shop now</a>
-                                        </div>
-                                    </div>
-                                    <div class="col-lg-6">
-                                        <div class="third-slider-img">
-                                            <div class="img-shape"
-                                                data-background="front_assets/img/slider/third_slide_shape.png"
-                                                data-animation-in="zoomIn" data-delay-in="1" data-duration-in="1.5">
-                                            </div>
-                                            <img src="front_assets/img/slider/third_slide_shape.png" alt=""
-                                                class="main-img" data-animation-in="slideInRight2" data-delay-in="1"
-                                                data-duration-in="1.5">
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="third-slider-item third-slider-bg"
-                        data-background="front_assets/img/slider/third_slider_bg.jpg">
-                        <div class="container custom-container-two">
-                            <div class="third-slider-wrap">
-                                <div class="row align-items-center">
-                                    <div class="col-lg-6">
-                                        <div class="slider-content">
-                                            <h3 class="sub-title" data-animation-in="fadeInUp" data-delay-in=".2"
-                                                data-duration-in="1.5">top deal !</h3>
-                                            <h2 class="title" data-animation-in="fadeInUp" data-delay-in=".4"
-                                                data-duration-in="1.5">New sports shoes</h2>
-                                            <p data-animation-in="fadeInUp" data-delay-in=".6" data-duration-in="1.5">
-                                                Get up
-                                                to 50% off Today Only</p>
-                                            <a href="shop-sidebar.html" class="btn" data-animation-in="fadeInUp"
-                                                data-delay-in=".8" data-duration-in="1.5">Shop now</a>
-                                        </div>
-                                    </div>
-                                    <div class="col-lg-6">
-                                        <div class="third-slider-img">
-                                            <div class="img-shape"
-                                                data-background="front_assets/img/slider/third_slide_shape.png"
-                                                data-animation-in="zoomIn" data-delay-in="1" data-duration-in="1.5">
-                                            </div>
-                                            <img src="front_assets/img/slider/third_slider_img03.png" alt=""
-                                                class="main-img" data-animation-in="slideInRight2" data-delay-in="1"
-                                                data-duration-in="1.5">
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="third-slider-item third-slider-bg"
-                        data-background="front_assets/img/slider/third_slider_bg.jpg">
-                        <div class="container custom-container-two">
-                            <div class="third-slider-wrap">
-                                <div class="row align-items-center">
-                                    <div class="col-lg-6">
-                                        <div class="slider-content">
-                                            <h3 class="sub-title" data-animation-in="fadeInUp" data-delay-in=".2"
-                                                data-duration-in="1.5">best offer !</h3>
-                                            <h2 class="title" data-animation-in="fadeInUp" data-delay-in=".4"
-                                                data-duration-in="1.5">Gym sports shoes</h2>
-                                            <p data-animation-in="fadeInUp" data-delay-in=".6" data-duration-in="1.5">
-                                                Get up
-                                                to 50% off Today Only</p>
-                                            <a href="shop-sidebar.html" class="btn" data-animation-in="fadeInUp"
-                                                data-delay-in=".8" data-duration-in="1.5">Shop now</a>
-                                        </div>
-                                    </div>
-                                    <div class="col-lg-6">
-                                        <div class="third-slider-img">
-                                            <div class="img-shape"
-                                                data-background="front_assets/img/slider/third_slide_shape.png"
-                                                data-animation-in="zoomIn" data-delay-in="1" data-duration-in="1.5">
-                                            </div>
-                                            <img src="front_assets/img/slider/third_slider_img04.png" alt=""
-                                                class="main-img" data-animation-in="slideInRight2" data-delay-in="1"
+                                            <img :src="item.image" alt="" class="main-img"
+                                                data-animation-in="slideInRight2" data-delay-in="1"
                                                 data-duration-in="1.5">
                                         </div>
                                     </div>
