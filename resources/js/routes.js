98fa1ec94d9cf641878221667e62edae38db54cb
Samiul Islam <samiulislamsharan@gmail.com>
2025-02-03 14:47:14 +0600

    feat(vue-router): :sparkles: add route for Category component with optional slug

--- resources/js/routes.js
+++ resources/js/routes.js
@@ -1,12 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Index from './frontend/Index.vue';
+import Category from './frontend/Category.vue';
 
 const routes = [
     {
         name: 'index',
         path: '/',
         component: Index
-    }
+    },
+    {
+        name: 'Category',
+        path: '/category/:slug?',
+        component: Category
+    },
 ];
 
 const router = createRouter({
