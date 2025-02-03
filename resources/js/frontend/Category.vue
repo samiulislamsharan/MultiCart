316ec76e2af927da7c15e11ca393190e08824644
Samiul Islam <samiulislamsharan@gmail.com>
2025-02-03 14:45:42 +0600

    feat(views): :sparkles: add Category component with layout integration

---
+++ resources/js/frontend/Category.vue
@@ -0,0 +1,19 @@
+<template>
+    <Layout>
+        <template v-slot:content>
+            <h1>Selected Category Page</h1>
+        </template>
+    </Layout>
+</template>
+<script>
+import Layout from './Layout.vue';
+import axios from 'axios';
+import getUrlList from '../provider.js';
+
+export default {
+    name: 'Category',
+    components: {
+        Layout
+    }
+}
+</script>
