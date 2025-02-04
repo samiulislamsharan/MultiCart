71f82748ef842225d9b7f520804be42e079c6914
Samiul Islam <samiulislamsharan@gmail.com>
2025-02-05 00:50:16 +0600

    feat(components): :sparkles: add HomeBrands component for displaying brand images in a responsive layout

---
+++ resources/js/frontend/components/HomeBrands.vue
@@ -0,0 +1,30 @@
+<template>
+    <!-- brand-area -->
+    <div class="instagram-post-area">
+        <div class="container-fluid p-0 fix">
+            <div class="row no-gutters insta-active">
+                <div v-for="item in homeBrands" :key="item.id" class="col">
+                    <div class="insta-post-item">
+                        <a href="#">
+                            <i class="fab fa-instagram"></i>
+                            <img :src="item.image" :alt="item.text + '_image'"
+                                style="height: 20rem ; object-fit: contain;">
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- brand-area-end -->
+</template>
+
+<script>
+export default {
+    props: {
+        homeBrands: {
+            type: Array,
+            required: true
+        }
+    }
+}
+</script>
