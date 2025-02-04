25af0cb2ca1c738a1cf1b01d19b72b1cd366a8b5
Samiul Islam <samiulislamsharan@gmail.com>
2025-02-05 00:50:00 +0600

    feat(views): :sparkles: replace new arrival section with NewArrivalProducts component for improved modularity in Index.vue

--- resources/js/frontend/Index.vue
+++ resources/js/frontend/Index.vue
@@ -14,51 +14,7 @@
             <!-- trending-product-area-end -->
 
             <!-- new-arrival-area -->
-            <section class="new-arrival-area pt-95 pb-45">
-                <div class="container">
-                    <div class="row justify-content-center">
-                        <div class="col-xl-4 col-lg-6">
-                            <div class="section-title title-style-two text-center mb-45">
-                                <h3 class="title">New Arrival Collection</h3>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="row justify-content-center">
-                        <div class="col-lg-6">
-                            <div class="new-arrival-nav mb-35">
-                                <button>Best Sellers</button>
-                                <button v-for="(item, index) in homeCategories" :key="item.id"
-                                    :class="showActiveClass(1, index)" :data-filter="'.category-' + item.id">
-                                    {{ item.name }}
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="row new-arrival-active">
-                        <div v-for="item in homeProducts" :key="item.id"
-                            :class="'col-xl-3 col-lg-4 col-sm-6 grid-item grid-sizer ' + 'category-' + item.category_id">
-                            <div class="new-arrival-item text-center mb-50">
-                                <div class="thumb mb-25">
-                                    <!-- <div class="discount-tag">-20%</div> -->
-                                    <div class="discount-tag new">New</div>
-                                    <a href="shop-details.html"><img :src="item.image" :alt="item.name + '_image'"
-                                            style="height: 15rem; object-fit: cover;"></a>
-                                    <div class="product-overlay-action">
-                                        <ul>
-                                            <li><a href="cart.html"><i class="far fa-heart"></i></a></li>
-                                            <li><a href="shop-details.html"><i class="far fa-eye"></i></a></li>
-                                        </ul>
-                                    </div>
-                                </div>
-                                <div class="content">
-                                    <h5><a href="shop-details.html">{{ item.name }}</a></h5>
-                                    <span class="price">{{ 'BDT ' + item.product_attributes[0].price }}</span>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </section>
+            <NewArrivalProducts :homeProducts="homeProducts" :homeCategories="homeCategories" />
             <!-- new-arrival-area-end -->
 
             <!-- shoes-banner-area -->
@@ -175,6 +131,7 @@ import getUrlList from '../provider';
 import HomeBanner from './components/HomeBanner.vue';
 import HomeCategories from './components/HomeCategories.vue';
 import TrendingProducts from './components/TrendingProducts.vue';
+import NewArrivalProducts from './components/NewArrivalProducts.vue';
 
 export default {
     name: 'Index',
@@ -183,6 +140,7 @@ export default {
         HomeBanner,
         HomeCategories,
         TrendingProducts,
+        NewArrivalProducts,
     },
     data() {
         return {
