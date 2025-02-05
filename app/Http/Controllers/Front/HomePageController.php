a92acc97eaed976bbb099677b0ccd3669fe0dec9
Samiul Islam <samiulislamsharan@gmail.com>
2025-02-05 20:59:07 +0600

    feat(controllers): :sparkles: add categoryIndex method to fetch category details and associated products in HomePageController.php

--- app/Http/Controllers/Front/HomePageController.php
+++ app/Http/Controllers/Front/HomePageController.php
@@ -7,6 +7,10 @@ use App\Models\Brand;
 use App\Models\Category;
 use App\Models\HomeBanner;
 use App\Models\Product;
+use App\Models\Size;
+use App\Models\Color;
+use App\Models\ProductAttr;
+use App\Models\CategoryAttribute;
 use App\Traits\ApiResponse;
 use Illuminate\Http\Request;
 
@@ -46,5 +50,90 @@ class HomePageController extends Controller
         $data['categories'] = Category::with('subcategories')->where('parent_category_id', null)->get();
 
         return $this->success(['data' => $data], 'Categories fetched successfully');
+    }
+
+    public function categoryIndex($slug = '')
+    {
+        if (!empty($slug)) {
+            $slug_category = Category::query()
+                ->where('slug', $slug)
+                ->first();
+
+            if (isset($slug_category)) {
+                $products = Product::query()
+                    ->where('category_id', $slug_category->id)
+                    ->with('productAttributes')
+                    ->select(
+                        'id',
+                        'name',
+                        'slug',
+                        'image',
+                        'item_code',
+                    )
+                    ->paginate(12);
+
+                $data = Category::query()
+                    ->where('slug', $slug)
+                    ->with('products:id,category_id,name,slug,image,item_code',)
+                    ->get();
+
+                if ($slug_category->parent_category_id == null || $slug_category->parent_category_id == '') {
+                    // get parent categories
+                    $categories = Category::query()
+                        ->where('parent_category_id', $slug_category->id)
+                        ->get();
+                } else {
+                    // get child categories
+                    $categories = Category::query()
+                        ->where('parent_category_id', $slug_category->parent_category_id)
+                        ->where('id', '!=', $slug_category->id)
+                        ->get();
+                }
+            } else {
+                $slug_category = Category::where('slug', $slug)->first();
+
+                $products = Product::query()
+                    ->where('category_id', $slug_category->id)
+                    ->with('productAttributes')
+                    ->select(
+                        'id',
+                        'name',
+                        'slug',
+                        'image',
+                        'item_code',
+                    )
+                    ->paginate(12);
+
+                $data = Category::query()
+                    ->where('slug', $slug)
+                    ->with('products:id,category_id,name,slug,image,item_code',)
+                    ->get();
+
+                if ($slug_category->parent_category_id == null || $slug_category->parent_category_id == '') {
+                    // get parent categories
+                    $categories = Category::query()
+                        ->where('parent_category_id', $slug_category->id)
+                        ->get();
+                } else {
+                    // get child categories
+                    $categories = Category::query()
+                        ->where('parent_category_id', $slug_category->parent_category_id)
+                        ->where('id', '!=', $slug_category->id)
+                        ->get();
+                }
+            }
+
+            $lowPrice = ProductAttr::orderBy('price', 'asc')->pluck('price')->first();
+            $highPrice = ProductAttr::orderBy('price', 'desc')->pluck('price')->first();
+
+            $brands = Brand::get();
+            $sizes = Size::get();
+            $colors = Color::get();
+            $attributes = CategoryAttribute::where('category_id', $slug_category->id)->with('attribute')->get();
+
+            return $this->success(['data' => get_defined_vars()], 'Categories fetched successfully');
+        } else {
+            return $this->error('Category not found', 404);
+        }
     }
 }
