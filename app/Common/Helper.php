a5e9ea67c3c792b2f4af27af2c204c1b24dc8934
Samiul Islam <samiulislamsharan@gmail.com>
2025-02-05 13:59:47 +0600

    feat(helpers): :sparkles: add replace_slug_str function for string formatting in Helper.php

--- app/Common/Helper.php
+++ app/Common/Helper.php
@@ -6,3 +6,14 @@ function prx($arr)
     print_r($arr);
     echo "</pre>";
 }
+
+/**
+ * make the string lowercase
+ * then replace spaces with hyphens
+ */
+function replace_slug_str($str)
+{
+    $str = strtolower($str);
+    $str = preg_replace('/\s+/', '-', $str);
+    return $str;
+}
