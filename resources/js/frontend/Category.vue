501f4f709d17d492d60642eb01b0af8a5399d17d
Samiul Islam <samiulislamsharan@gmail.com>
2025-02-05 21:04:48 +0600

    feat(views): :sparkles: implement product fetching logic in Category.vue using slug from route

--- resources/js/frontend/Category.vue
+++ resources/js/frontend/Category.vue
@@ -9,6 +9,7 @@
 import Layout from './Layout.vue';
 import axios from 'axios';
 import getUrlList from '../provider.js';
+import { useRoute } from 'vue-router';
 
 export default {
     name: 'Category',
@@ -29,6 +30,35 @@ export default {
             categoriesProductCount: [],
         }
     },
+    mounted() {
+        this.getProducts();
+    },
+    methods: {
+        async getProducts() {
+            try {
+                const route = useRoute();
+                this.slug = this.$route.params.slug;
+
+                let data = await axios.get(getUrlList().category + '/' + this.slug);
+
+                if (this.slug == '' || this.slug == undefined || this.slug == null) {
+                    this.$router.push({ name: 'Index' });
+                } else {
+                    if (data.status == 200 && data.data.data.data.categories.length > 0) {
+                        this.categories = data.data.data.data.categories;
+                        this.products = data.data.data.data.products.data;
+                        this.brands = data.data.data.data.brands;
+                        this.colors = data.data.data.data.colors;
+                        this.sizes = data.data.data.data.sizes;
+                        this.categoriesProductCount = data.data.data.data.categories_product_count;
+                    } else {
+                        console.log('No data found');
+                    }
+                }
+            } catch (error) {
+                console.log(error);
+            }
+        }
     }
 }
 </script>
