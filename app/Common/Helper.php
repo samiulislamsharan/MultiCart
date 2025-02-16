c92e336fda57c4414ce93107e9de6e313ace3611
Samiul Islam <samiulislamsharan@gmail.com>
2025-02-17 01:25:20 +0600

    feat(helpers): :sparkles: add generateRandomString function for random string generation

--- app/Common/Helper.php
+++ app/Common/Helper.php
@@ -33,3 +33,15 @@ function checkTokenExpiryInMinutes($time, $timeDiff = 60)
         return false;
     }
 }
+
+function generateRandomString($length = 20)
+{
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $charactersLength = strlen($characters);
+    $randomString = '';
+
+    for ($i = 0; $i < $length; $i++) {
+        $randomString .= $characters[rand(0, $charactersLength - 1)];
+    }
+    return $randomString;
+}
