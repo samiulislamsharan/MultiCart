401fc3f305ffba7f66f65d48740984e1d4789823
Samiul Islam <samiulislamsharan@gmail.com>
2025-02-20 23:41:43 +0600

    style(views): :art: improve formatting and add titles for action links in NewArrivalProducts.vue

--- resources/js/frontend/components/NewArrivalProducts.vue
+++ resources/js/frontend/components/NewArrivalProducts.vue
@@ -27,18 +27,32 @@
                         <div class="thumb mb-25">
                             <!-- <div class="discount-tag">-20%</div> -->
                             <div class="discount-tag new">New</div>
-                            <a href="shop-details.html"><img :src="item.image" :alt="item.name + '_image'"
-                                    style="height: 15rem; object-fit: cover;"></a>
+                            <a href="shop-details.html">
+                                <img :src="item.image" :alt="item.name + '_image'"
+                                    style="height: 15rem; object-fit: cover;">
+                            </a>
                             <div class="product-overlay-action">
                                 <ul>
-                                    <li><a href="cart.html"><i class="far fa-heart"></i></a></li>
-                                    <li><a href="shop-details.html"><i class="far fa-eye"></i></a></li>
+                                    <li>
+                                        <a href="cart.html" title="Add to wishlist">
+                                            <i class="far fa-heart"></i>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="shop-details.html" title="View product">
+                                            <i class="far fa-eye"></i>
+                                        </a>
+                                    </li>
                                 </ul>
                             </div>
                         </div>
                         <div class="content">
-                            <h5><a href="shop-details.html">{{ item.name }}</a></h5>
-                            <span class="price">{{ 'BDT ' + formatPrice(item.product_attributes[0].price) }}</span>
+                            <h5>
+                                <a href="shop-details.html">{{ item.name }}</a>
+                            </h5>
+                            <span class="price">
+                                {{ 'BDT ' + formatPrice(item.product_attributes[0].price) }}
+                            </span>
                         </div>
                     </div>
                 </div>
