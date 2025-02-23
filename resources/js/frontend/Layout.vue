06a99f2ff016e51060592442254da749a694f75b
Samiul Islam <samiulislamsharan@gmail.com>
2025-02-23 15:21:00 +0600

    fix(views): :bug: handle errors using try-catch block on getUser function in Layout.vue

--- resources/js/frontend/Layout.vue
+++ resources/js/frontend/Layout.vue
@@ -518,15 +518,20 @@ export default {
             }
         },
         async getUser() {
-            if (localStorage.getItem('user_info')) {
-                var user = localStorage.getItem('user_info');
-                var testUser = JSON.parse(user);
+            try {
+                if (localStorage.getItem('user_info')) {
+                    var user = localStorage.getItem('user_info');
+                    var testUser = JSON.parse(user);
 
-                this.user_info.user_id = testUser.user_id;
+                    this.user_info.user_id = testUser.user_id;
 
-                this.getUserData();
-            } else {
-                this.getUserData();
+                    this.getUserData();
+                } else {
+                    this.getUserData();
+                }
+            }
+            catch (error) {
+                console.error(error);
             }
         },
         async getUserData() {
