67a775a59a16f3a8c41badf2861209cdfb8ea118
Samiul Islam <samiulislamsharan@gmail.com>
2025-02-24 00:52:09 +0600

    feat(components): :sparkles: refactor cart functionality into HeaderCart component for better code organization

--- resources/js/frontend/Layout.vue
+++ resources/js/frontend/Layout.vue
@@ -91,70 +91,8 @@
                                 <!-- navbar end -->
 
                                 <!-- cart -->
-                                <div class="header-action d-none d-md-block">
-                                    <ul>
-                                        <li class="header-search">
-                                            <a href="#" data-toggle="modal" data-target="#search-modal">
-                                                <i class="flaticon-search"></i>
-                                            </a>
-                                        </li>
-                                        <li class="header-shop-cart">
-                                            <a href="#">
-                                                <i class="flaticon-shopping-bag"></i>
-                                                <span>{{ cartCount }}</span>
-                                            </a>
-                                            <ul class="minicart">
-                                                <li v-if="cartCount > 0" v-for="item in cartProducts" :key="item.id"
-                                                    class="d-flex align-items-start">
-                                                    <div class="cart-img">
-                                                        <a href="#">
-                                                            <img :src="item.products[0].image"
-                                                                :alt="item.products[0].slug + '_image'"
-                                                                style="height: 5rem; object-fit: cover;">
-                                                        </a>
-                                                    </div>
-                                                    <div class="cart-content">
-                                                        <h4><a href="#">{{ item.products[0].name }}</a></h4>
-                                                        <div class="cart-price">
-                                                            <span class="new">{{
-                                                                '&#2547; ' +
-                                                                formatPrice(item.products[0].product_attributes[0].price)
-                                                                }}</span>
-                                                            <span>
-                                                                <del>
-                                                                    {{ '&#2547; ' +
-                                                                        formatPrice(item.products[0].product_attributes[0].mrp)
-                                                                    }}
-                                                                </del>
-                                                            </span>
-                                                        </div>
-                                                    </div>
-                                                    <div class="del-icon">
-                                                        <a @click="removeFromCart(item.products[0].id, item.products[0].product_attributes[0].id, 1)"
-                                                            href="javascript:void(0)"><i
-                                                                class="far fa-trash-alt"></i></a>
-                                                    </div>
-                                                </li>
-                                                <li>
-                                                    <div class="total-price">
-                                                        <span class="f-left">Total:</span>
-                                                        <span class="f-right">
-                                                            {{ '&#2547; ' + formatPrice(cartTotal) }}
-                                                        </span>
-                                                    </div>
-                                                </li>
-                                                <li>
-                                                    <div class="checkout-link">
-                                                        <a href="#">Shopping Cart</a>
-                                                        <a class="black-color" href="#">Checkout</a>
-                                                    </div>
-                                                </li>
-                                            </ul>
-                                        </li>
-                                        <li class="header-profile"><a href="#"><i class="flaticon-user-profile"></i></a>
-                                        </li>
-                                    </ul>
-                                </div>
+                                <HeaderCart :cartCount="cartCount" :cartProducts="cartProducts" :cartTotal="cartTotal"
+                                    :removeFromCart="removeFromCart" :formatPrice="formatPrice"/>
                                 <!-- cart end -->
                             </nav>
                         </div>
@@ -387,9 +325,13 @@
 <script>
 import axios from 'axios';
 import getUrlList from '../provider.js';
+import HeaderCart from './components/HeaderCart.vue';
 
 export default {
     name: 'Layout',
+    components: {
+        HeaderCart
+    },
     data() {
         return {
             result: [],
---
+++ resources/js/frontend/components/HeaderCart.vue
@@ -0,0 +1,94 @@
+<template>
+    <!-- cart -->
+    <div class="header-action d-none d-md-block">
+        <ul>
+            <li class="header-search">
+                <a href="#" data-toggle="modal" data-target="#search-modal">
+                    <i class="flaticon-search"></i>
+                </a>
+            </li>
+            <li class="header-shop-cart">
+                <a href="#">
+                    <i class="flaticon-shopping-bag"></i>
+                    <span>{{ cartCount }}</span>
+                </a>
+                <ul class="minicart">
+                    <li v-if="cartCount > 0" v-for="item in cartProducts" :key="item.id"
+                        class="d-flex align-items-start">
+                        <div class="cart-img">
+                            <a href="#">
+                                <img :src="item.products[0].image" :alt="item.products[0].slug + '_image'"
+                                    style="height: 5rem; object-fit: cover;">
+                            </a>
+                        </div>
+                        <div class="cart-content">
+                            <h4><a href="#">{{ item.products[0].name }}</a></h4>
+                            <div class="cart-price">
+                                <span class="new">{{
+                                    '&#2547; ' +
+                                    formatPrice(item.products[0].product_attributes[0].price)
+                                }}</span>
+                                <span>
+                                    <del>
+                                        {{ '&#2547; ' +
+                                            formatPrice(item.products[0].product_attributes[0].mrp)
+                                        }}
+                                    </del>
+                                </span>
+                            </div>
+                        </div>
+                        <div class="del-icon">
+                            <a @click="removeFromCart(item.products[0].id, item.products[0].product_attributes[0].id, 1)"
+                                href="javascript:void(0)"><i class="far fa-trash-alt"></i></a>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="total-price">
+                            <span class="f-left">Total:</span>
+                            <span class="f-right">
+                                {{ '&#2547; ' + formatPrice(cartTotal) }}
+                            </span>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="checkout-link">
+                            <a href="#">Shopping Cart</a>
+                            <a class="black-color" href="#">Checkout</a>
+                        </div>
+                    </li>
+                </ul>
+            </li>
+            <li class="header-profile"><a href="#"><i class="flaticon-user-profile"></i></a>
+            </li>
+        </ul>
+    </div>
+    <!-- cart end -->
+</template>
+
+<script>
+export default {
+    name: 'HeaderCart',
+    props: {
+        cartProducts: {
+            type: Array,
+            required: true
+        },
+        cartCount: {
+            type: Number,
+            required: true
+        },
+        cartTotal: {
+            type: Number,
+            required: true
+        },
+        formatPrice: {
+            type: Function,
+            required: true
+        },
+        removeFromCart: {
+            type: Function,
+            required: true
+        },
+    },
+}
+</script>
