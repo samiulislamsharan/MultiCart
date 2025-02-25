f017bb8dab41260e0a496173592dac7eeab285e9
Samiul Islam <samiulislamsharan@gmail.com>
2025-02-26 00:19:08 +0600

    feat(views): :sparkles: update Product.vue to display dynamic product name and price, and replace placeholder text with product description

--- resources/js/frontend/Product.vue
+++ resources/js/frontend/Product.vue
@@ -70,8 +70,8 @@
                         </div>
                         <div class="col-lg-5">
                             <div class="shop-details-content">
-                                <a href="#" class="product-cat">Tracker Jacket</a>
-                                <h3 class="title">Woman Tracker Jacket</h3>
+                                <a href="#" class="product-cat">Product Category</a>
+                                <h3 class="title">{{ product.name }}</h3>
                                 <div class="rating">
                                     <i class="fas fa-star"></i>
                                     <i class="fas fa-star"></i>
@@ -79,8 +79,11 @@
                                     <i class="fas fa-star"></i>
                                     <i class="fas fa-star"></i>
                                 </div>
-                                <p class="style-name">Style Name : TN-WI56-OMTJ-CqTKJ-09#</p>
-                                <div class="price">Price : $ 29.00</div>
+                                <p class="style-name">Item Code : {{ product.item_code }}</p>
+                                <div class="price">
+                                    Price : &#2547;
+                                    {{ product.product_attributes[0].price }}
+                                </div>
                                 <div class="product-details-info">
                                     <span>Size <a href="#">Guide</a></span>
                                     <div class="sidebar-product-size mb-30">
@@ -160,79 +163,7 @@
                                         <div class="product-desc-title mb-30">
                                             <h4 class="title">Additional information :</h4>
                                         </div>
-                                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                                            tempor
-                                            incididunt ut labore et dolore magna
-                                            aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                                            nisi
-                                            ut aliquip ex ea commodo consequat. Duis
-                                            aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                                            fugiat nulla pariatur. Excepteur sint
-                                            occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
-                                            anim
-                                            id est laborum.</p>
-                                        <p>The purpose of lorem ipsum is to create a natural looking block of text
-                                            (sentence, paragraph, page, etc.) that doesn't
-                                            distract from the layout. A practice not without controversy, laying out
-                                            pages
-                                            with meaningless filler text can be very
-                                            useful when the focus is meant to be on design, not content.</p>
-                                        <div class="color-size-info">
-                                            <ul>
-                                                <li><span>COLOR :</span> Black, Gray</li>
-                                                <li><span>SIZE :</span> XS, S, M, L</li>
-                                            </ul>
-                                        </div>
-                                        <div class="additional-table">
-                                            <div class="table-responsive">
-                                                <table class="table table-bordered">
-                                                    <tbody>
-                                                        <tr>
-                                                            <th scope="row">Size Name</th>
-                                                            <td>28</td>
-                                                            <td>49</td>
-                                                            <td>36</td>
-                                                            <td>55</td>
-                                                            <td>44</td>
-                                                            <td>34</td>
-                                                        </tr>
-                                                        <tr>
-                                                            <th scope="row">Waist Stretch</th>
-                                                            <td>19</td>
-                                                            <td>38</td>
-                                                            <td>31</td>
-                                                            <td>55</td>
-                                                            <td>44</td>
-                                                            <td>34</td>
-                                                        </tr>
-                                                        <tr>
-                                                            <th scope="row">Hip (7½” below from waist)</th>
-                                                            <td>11</td>
-                                                            <td>18</td>
-                                                            <td>21</td>
-                                                            <td>55</td>
-                                                            <td>44</td>
-                                                            <td>34</td>
-                                                        </tr>
-                                                        <tr>
-                                                            <th scope="row">length (Out seam)</th>
-                                                            <td>28</td>
-                                                            <td>31</td>
-                                                            <td>19</td>
-                                                            <td>55</td>
-                                                            <td>44</td>
-                                                            <td>34</td>
-                                                        </tr>
-                                                    </tbody>
-                                                </table>
-                                            </div>
-                                        </div>
-                                        <p>The purpose of lorem ipsum is to create a natural looking block of text
-                                            (sentence, paragraph, page, etc.) that doesn't
-                                            distract from the layout. A practice not without controversy, laying out
-                                            pages
-                                            with meaningless filler text can be very
-                                            useful when the focus is meant to be on design, not content.</p>
+                                        <div v-html="product.description"></div>
                                     </div>
                                     <div class="tab-pane fade" id="reviews" role="tabpanel"
                                         aria-labelledby="reviews-tab">
