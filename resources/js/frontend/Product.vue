41d79291ba4fef59922233cfa9d002dfc8bd96da
Samiul Islam <samiulislamsharan@gmail.com>
2025-03-01 14:59:56 +0600

    fix(views): :bug: format price display in Product.vue component using slotProps.formatPrice

--- resources/js/frontend/Product.vue
+++ resources/js/frontend/Product.vue
@@ -81,7 +81,7 @@
                                 </div>
                                 <p class="style-name">Item Code : {{ product.item_code }}</p>
                                 <div class="price">
-                                    {{ 'Price : &#2547; ' + product.product_attributes[0].price }}
+                                    {{ 'Price : &#2547; ' + slotProps.formatPrice(product.product_attributes[0].price) }}
                                 </div>
                                 <div class="product-details-info">
                                     <span>Size <a href="#">Guide</a></span>
@@ -251,7 +251,7 @@
                                     <div class="content">
                                         <h5><a href="#">{{ item.name }}</a></h5>
                                         <span class="price">
-                                            {{ '&#2547; ' + product.product_attributes[0].price }}
+                                            {{ '&#2547; ' + slotProps.formatPrice(product.product_attributes[0].price) }}
                                         </span>
                                     </div>
                                 </div>
