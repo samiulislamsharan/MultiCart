bd63c158c58dca6fd734dc114ba2c2d4346cc326
Samiul Islam <samiulislamsharan@gmail.com>
2025-03-02 23:00:56 +0600

    feat(migrations): :sparkles: create coupons table for managing discount codes

---
+++ database/migrations/2025_03_01_154503_create_coupons_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('coupons', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->nullable();
+            $table->enum('type', ['1', '2'])->default('1')->comment('1: Value, 2: Percentage');
+            $table->integer('value')->default(0);
+            $table->integer('min_value')->default(0);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('coupons');
+    }
+};
