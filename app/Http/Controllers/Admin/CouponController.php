e73092a223cc66885232f60ed1963da8650e6da8
Samiul Islam <samiulislamsharan@gmail.com>
2025-03-02 23:02:30 +0600

    feat(controllers): :sparkles: implement index and store methods in CouponController for managing coupons

--- app/Http/Controllers/Admin/CouponController.php
+++ app/Http/Controllers/Admin/CouponController.php
@@ -11,4 +11,48 @@ use Illuminate\Support\Facades\Validator;
 class CouponController extends Controller
 {
     use ApiResponse;
+
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $data = Coupon::get();
+
+        return view('admin.coupons.index', get_defined_vars());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        try {
+            $validation = Validator::make($request->all(), [
+                'id' => 'required',
+                'name' => 'required|string|max:255',
+                'type' => 'required|string|in:1,2',
+                'value'=> 'required|numeric',
+                'min_value'=> 'required|numeric',
+            ]);
+
+            if ($validation->fails()) {
+                return $this->error($validation->errors(), 422, []);
+            } else {
+                Coupon::updateOrCreate(
+                    ['id' => $request->id],
+                    [
+                        'name' => $request->name,
+                        'type' => $request->type,
+                        'value' => $request->value,
+                        'min_value' => $request->min_value,
+                    ]
+                );
+
+                return $this->success(['reload' => true], 'Coupon updated successfully.');
+            }
+        } catch (\Exception $e) {
+            return $this->error($e->getMessage(), 500, []);
+        }
+    }
 }
