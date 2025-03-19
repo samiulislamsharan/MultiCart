095d8d8505d7e0171193b4100490e0203b405adc
Samiul Islam <samiulislamsharan@gmail.com>
2025-03-19 16:51:59 +0600

    feat(models): :sparkles: create UserAddress model for managing user address data

---
+++ app/Models/UserAddress.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class UserAddress extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'post_code',
+        'city',
+        'division',
+        'address',
+        'country',
+    ];
+}
